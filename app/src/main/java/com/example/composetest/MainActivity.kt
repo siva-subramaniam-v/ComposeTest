20989fcf202635f1d415b96824ede0d7a14fdb06
Siva <doodsiva@gmail.com>
2023-01-05 13:14:24 +0530

    First Composable Function

---
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.composetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composetest.ui.theme.ComposeTestTheme
+import com.example.composetest.ui.theme.Typography
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column {
+                        Greeting("Android")
+                        CustomText(text = "Stevdza-San")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun CustomText(text: String) {
+    Text(
+        text = text,
+        style = Typography.h5
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTestTheme {
+        Column {
+            Greeting("Android")
+            CustomText(text = "Stevdza-San")
+        }
+    }
+}
