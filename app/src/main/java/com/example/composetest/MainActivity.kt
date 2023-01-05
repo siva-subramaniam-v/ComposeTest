c0c1471d6ae7d1a8cce9977976dacd10e1af3f89
Siva <doodsiva@gmail.com>
2023-01-05 18:44:06 +0530

    Rows and Columns

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
-import com.example.composetest.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +22,17 @@ class MainActivity : ComponentActivity() {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        CustomText(text = "Stevdza-San")
+                    Row(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+                        CustomItem(weight = 1f)
                     }
                 }
             }
@@ -35,25 +41,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun CustomText(text: String) {
-    Text(
-        text = text,
-        style = Typography.h5
-    )
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTestTheme {
-        Column {
-            Greeting("Android")
-            CustomText(text = "Stevdza-San")
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+            CustomItem(weight = 1f)
         }
     }
 }
