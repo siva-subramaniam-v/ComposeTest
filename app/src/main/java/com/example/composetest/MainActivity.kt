cb6fdeebb247f05c0854ff80a8c245d8df864286
Siva <doodsiva@gmail.com>
2023-01-06 12:24:19 +0530

    Superscript/Subscript Text

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -3,29 +3,20 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.TextUnit
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,17 +26,9 @@ class MainActivity : ComponentActivity() {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Text(
-                            text = stringResource(id = R.string.app_name),
-                            modifier = Modifier
-                                .background(MaterialTheme.colors.primary)
-                                .padding(16.dp),
-                            color = Color.White
-                        )
-                    }
                 }
             }
         }
@@ -53,47 +36,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CustomText() {
-    Text(
-        text = stringResource(id = R.string.app_name),
-        modifier = Modifier
-            .background(MaterialTheme.colors.primary)
-            .padding(16.dp)
-            .width(200.dp),
-        color = Color.White,
-        fontSize = MaterialTheme.typography.h6.fontSize,
-        fontStyle = FontStyle.Italic,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center
-    )
-}
-
-@Composable
-fun CustomText2() {
+fun SuperScriptText(
+    normalText: String,
+    normalFontSize: TextUnit = MaterialTheme.typography.subtitle1.fontSize,
+    superText: String,
+    superTextFontSize: TextUnit = MaterialTheme.typography.overline.fontSize,
+    superTextFontWeight: FontWeight = FontWeight.Normal
+) {
     Text(
         buildAnnotatedString {
-            withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)) {
-                withStyle(
-                    style = SpanStyle(
-                        color = MaterialTheme.colors.primary,
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                ) {
-                    append("A")
-                }
-
-                append("B")
-                append("C")
-                append("D")
+            withStyle(
+                style = SpanStyle(
+                    fontSize = normalFontSize
+                )
+            ) {
+                append(normalText)
             }
-        }, modifier = Modifier.width(200.dp)
-    )
-}
 
-@Composable
-fun CustomText3() {
-    Text(text = "Hello World!".repeat(20), maxLines = 2, overflow = TextOverflow.Ellipsis)
+            withStyle(
+                style = SpanStyle(
+                    fontSize = superTextFontSize,
+                    fontWeight = superTextFontWeight,
+                    baselineShift = BaselineShift.Superscript // BaselineShift.Subscript
+                )
+            ) {
+                append(superText)
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
@@ -101,7 +71,11 @@ fun CustomText3() {
 fun DefaultPreview() {
     ComposeTestTheme {
         Column(modifier = Modifier.fillMaxSize()) {
-            CustomText3()
+            SuperScriptText(
+                normalText = "Hello",
+                superText = "World!",
+                superTextFontWeight = FontWeight.Light
+            )
         }
     }
 }
