71763223c8ad50acc55fd23a055a8f08570eb2b6
Siva <doodsiva@gmail.com>
2023-01-06 12:46:21 +0530

    Expandable Card With Animation

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -3,79 +3,55 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.secondary)
+                        .padding(24.dp)
                 ) {
+                    ExpandableCard(
+                        title = "My Title",
+                        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                                "Vestibulum pretium, sem ac pretium accumsan, nunc lectus" +
+                                "convallis ipsum, et feugiat libero erat sit amet est." +
+                                "Suspendisse tempor molestie tempor. Ut et arcu non sem dictum condimentum."
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun SuperScriptText(
-    normalText: String,
-    normalFontSize: TextUnit = MaterialTheme.typography.subtitle1.fontSize,
-    superText: String,
-    superTextFontSize: TextUnit = MaterialTheme.typography.overline.fontSize,
-    superTextFontWeight: FontWeight = FontWeight.Normal
-) {
-    Text(
-        buildAnnotatedString {
-            withStyle(
-                style = SpanStyle(
-                    fontSize = normalFontSize
-                )
-            ) {
-                append(normalText)
-            }
-
-            withStyle(
-                style = SpanStyle(
-                    fontSize = superTextFontSize,
-                    fontWeight = superTextFontWeight,
-                    baselineShift = BaselineShift.Superscript // BaselineShift.Subscript
-                )
-            ) {
-                append(superText)
-            }
-        }
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
+@ExperimentalMaterialApi
 fun DefaultPreview() {
     ComposeTestTheme {
-        Column(modifier = Modifier.fillMaxSize()) {
-            SuperScriptText(
-                normalText = "Hello",
-                superText = "World!",
-                superTextFontWeight = FontWeight.Light
-            )
-        }
+        ExpandableCard(
+            title = "My Title",
+            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Vestibulum pretium, sem ac pretium accumsan, nunc lectus" +
+                    "convallis ipsum, et feugiat libero erat sit amet est." +
+                    "Suspendisse tempor molestie tempor. Ut et arcu non sem dictum condimentum."
+        )
     }
 }
