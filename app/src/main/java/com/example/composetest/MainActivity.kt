c9bb6363d4896ee94b467c47a2e6a4616ec99f90
Siva <doodsiva@gmail.com>
2023-01-09 18:50:17 +0530

    Lazy Column

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -4,83 +4,76 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetest.repository.PersonRepository
 import com.example.composetest.ui.theme.ComposeTestTheme
-import androidx.compose.runtime.*
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+//                val personRepository = PersonRepository()
+//                val data = personRepository.getAllData()
+
+                val sections = listOf("A", "B", "C", "D", "E", "F", "G")
+
+                LazyColumn(
+                    contentPadding = PaddingValues(all = 12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    Greeting()
+//                    itemsIndexed(
+//                        items = data,
+//                        key = { index, person ->
+//                            person.id
+//                        }
+//                    ) { index, person ->
+//                        Log.d("MainActivity", index.toString())
+//                        CustomItem(person = person)
+//                    }
+                    sections.forEach { section ->
+                        stickyHeader {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.LightGray)
+                                    .padding(12.dp),
+                                text = "Section $section"
+                            )
+                        }
+
+                        items(10) {
+                            Text(
+                                modifier = Modifier.padding(12.dp),
+                                text = "Item $it from the section $section"
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var text by remember { mutableStateOf("Type here...") }
-
-        OutlinedTextField(
-            value = text,
-            onValueChange = { newText -> text = newText },
-            label = {
-            Text(text = "Email")
-        },leadingIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.Filled.Email, contentDescription = "Email Icon"
-                )
-            }
-        }, trailingIcon = {
-            IconButton(onClick = {
-                Log.d("Trailing Icon", "Clicked")
-            }) {
-                Icon(
-                    imageVector = Icons.Filled.Check, contentDescription = "Check Icon"
-                )
-            }
-        }, keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Search
-            ), keyboardActions = KeyboardActions(
-                onSearch = {
-                    Log.d("ImeAction", "Search Clicked")
-                }
-            )
-        )
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTestTheme {
-        Greeting()
     }
 }
