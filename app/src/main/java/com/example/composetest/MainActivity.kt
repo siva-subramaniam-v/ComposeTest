f87986cd6518e7ae27e4812b38e85f662234ff9e
Siva <doodsiva@gmail.com>
2023-01-09 11:42:04 +0530

    Text Fields

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,57 +1,86 @@
 package com.example.composetest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.secondary)
-                        .padding(24.dp)
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    ExpandableCard(
-                        title = "My Title",
-                        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
-                                "Vestibulum pretium, sem ac pretium accumsan, nunc lectus" +
-                                "convallis ipsum, et feugiat libero erat sit amet est." +
-                                "Suspendisse tempor molestie tempor. Ut et arcu non sem dictum condimentum."
-                    )
+                    Greeting()
                 }
             }
         }
     }
 }
 
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text by remember { mutableStateOf("Type here...") }
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            label = {
+            Text(text = "Email")
+        },leadingIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.Email, contentDescription = "Email Icon"
+                )
+            }
+        }, trailingIcon = {
+            IconButton(onClick = {
+                Log.d("Trailing Icon", "Clicked")
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Check, contentDescription = "Check Icon"
+                )
+            }
+        }, keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Search
+            ), keyboardActions = KeyboardActions(
+                onSearch = {
+                    Log.d("ImeAction", "Search Clicked")
+                }
+            )
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-@ExperimentalMaterialApi
 fun DefaultPreview() {
     ComposeTestTheme {
-        ExpandableCard(
-            title = "My Title",
-            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
-                    "Vestibulum pretium, sem ac pretium accumsan, nunc lectus" +
-                    "convallis ipsum, et feugiat libero erat sit amet est." +
-                    "Suspendisse tempor molestie tempor. Ut et arcu non sem dictum condimentum."
-        )
+        Greeting()
     }
 }
